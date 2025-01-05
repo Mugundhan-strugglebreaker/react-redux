5ed3e76bf68effc52f89a3d2271fc2a585c15d55
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-01-05 14:19:28 -0800

    counter component using dispatch and selector for store access

--- my-app/src/components/Counter.tsx
+++ my-app/src/components/Counter.tsx
@@ -1,9 +1,29 @@
 
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import { decrement, increment , incrementByAmont } from "../state/counter/counterSlice.ts";
 
 const Counter = () => {
+  const count = useSelector((state : RootState) => state.counter.value)
+  const dispatch = useDispatch();
+
   return (
-    <h1>Mugundhan S</h1>
+    <div>
+        <h2>{count}</h2>
+        <div>
+            <button onClick={() => dispatch(increment())}>
+                Increment
+            </button>
+            <button onClick={()=> dispatch(decrement())}>
+                Decrement
+            </button>
+
+            <button onClick={() => dispatch(incrementByAmont(10))}>
+                Increment By Amount
+            </button>
+        </div>
+    </div>
   )
 };
 export default Counter
